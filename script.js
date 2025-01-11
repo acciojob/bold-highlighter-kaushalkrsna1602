c2b5d8b462b9ec1b844c34b2bbbd3eab364c7497
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2025-01-11 14:04:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+let strong = document.querySelectorAll("strong")
 function highlight() {
     //Write your code here
-
-
+	strong.forEach(darkColor => {
+		darkColor.style.color = "orange"
+	})
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	strong.forEach(greenColor => {
+		greenColor.style.color = "black"
+	})
 }
